3691fd2672126b513883cce7f68c2f152ae3544c
Alsu <alsu.bikkulova@gmail.com>
2023-06-05 17:06:31 +0100

    Task completed

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,28 @@
 public class Main {
+    public static void main(String[] args) {
+        int onAccount = 300;
+        int replenishment = 1200;
+        int bonusPrice = 100;
+        int accountTotal;
+        int bonusAmount;
+
+        if(replenishment > 1000) {
+            bonusAmount = replenishment / bonusPrice;
+        } else {
+            bonusAmount = 0;
+        }
+        accountTotal = onAccount + replenishment + bonusAmount;
+
+        System.out.println("Бонус равен " + bonusAmount);
+        System.out.println("Итоговая сумма на счету клиента равна " + accountTotal);
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: начального счёта,
+        // суммы пополнения и тп
+
+        // Условным оператором проверяете, превысила ли
+        // сумма пополнения порог, и для этих двух разных
+        // сценариев рассчитываете сумму бонуса и выводите
+        // на экран.
+    }
 }
